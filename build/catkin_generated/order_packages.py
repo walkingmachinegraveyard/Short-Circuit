0f75fc07509da70f73ca6d366f8a126aa2ee379a
wmets <walkingmachine@gmail.com>
2015-10-20 22:40:14 -0400

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/wm/Short-Circuit/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/wm/catkin_ws/devel;/home/wm/Xsens_ws/install;/home/wm/jaco_ws/install;/home/wm/catkin_ws/install;/opt/ros/indigo".split(';') if "/home/wm/catkin_ws/devel;/home/wm/Xsens_ws/install;/home/wm/jaco_ws/install;/home/wm/catkin_ws/install;/opt/ros/indigo" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wm/Short-Circuit/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/wm/Short-Circuit/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/wm/Short-Circuit/devel/lib:/home/wm/Short-Circuit/devel/lib/x86_64-linux-gnu:/home/wm/catkin_ws/devel/lib/x86_64-linux-gnu:/home/wm/Xsens_ws/install/lib/x86_64-linux-gnu:/home/wm/jaco_ws/install/lib/x86_64-linux-gnu:/home/wm/catkin_ws/install/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/wm/catkin_ws/devel/lib:/home/wm/Xsens_ws/install/lib:/home/wm/jaco_ws/install/lib:/home/wm/catkin_ws/install/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/wm/Short-Circuit/devel/bin:/home/wm/catkin_ws/devel/bin:/home/wm/Xsens_ws/install/bin:/home/wm/jaco_ws/install/bin:/home/wm/catkin_ws/install/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/wm/Short-Circuit/devel/lib/pkgconfig:/home/wm/Short-Circuit/devel/lib/x86_64-linux-gnu/pkgconfig:/home/wm/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/wm/Xsens_ws/install/lib/x86_64-linux-gnu/pkgconfig:/home/wm/jaco_ws/install/lib/x86_64-linux-gnu/pkgconfig:/home/wm/catkin_ws/install/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/wm/catkin_ws/devel/lib/pkgconfig:/home/wm/Xsens_ws/install/lib/pkgconfig:/home/wm/jaco_ws/install/lib/pkgconfig:/home/wm/catkin_ws/install/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/wm/Short-Circuit/build"
+export PYTHONPATH="/home/wm/Short-Circuit/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/wm/Short-Circuit/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/wm/Short-Circuit/src:$ROS_PACKAGE_PATH"
