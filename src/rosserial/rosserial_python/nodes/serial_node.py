bfcbd30c586f973f944d591495b6779f6cc3c2e9
gortium <thierrypouplier@gmail.com>
2016-03-25 22:45:56 -0400

    Modification du port pour le arduino de la tete

--- src/rosserial/rosserial_python/nodes/serial_node.py
+++ src/rosserial/rosserial_python/nodes/serial_node.py
@@ -46,7 +46,7 @@ if __name__=="__main__":
     rospy.init_node("serial_node")
     rospy.loginfo("ROS Serial Python Node")
 
-    port_name = rospy.get_param('~port','/dev/ttyUSB0')
+    port_name = rospy.get_param('~port','/dev/ttyACM0')
     baud = int(rospy.get_param('~baud','57600'))
 
     # TODO: should these really be global?
